e3c734638f981d57d2973b46a5de64e101e41dec
Richy_D <1245413737@qq.com>
2022-07-06 23:36:24 +0800

    相关的配置

--- myblog/urls.py
+++ myblog/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
 from app import views
 
 urlpatterns = [
@@ -24,4 +25,5 @@ urlpatterns = [
     path('tpl/', views.tpl),
     path('login/', views.login),
     path('orm/', views.orm),
+    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
