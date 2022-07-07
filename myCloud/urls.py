47326e2df56309ff6f404fa951748c9cac223bdd
Richy_D <1245413737@qq.com>
2022-07-07 18:14:40 +0800

    主页面
    配置scss、主页面等内容

--- myCloud/urls.py
+++ myCloud/urls.py
@@ -20,6 +20,6 @@ from django.views.static import serve
 from app01 import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
     re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
