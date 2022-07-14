412e957372c89f19753e2923946c2dc4b430d25c
Richy_D <1245413737@qq.com>
2022-07-15 05:48:52 +0800

    登录验证
    简单的后端登录验证

--- app01/views.py
+++ app01/views.py
@@ -12,9 +12,31 @@ def News(request):
 
 def Login(request):
     if request.method == 'POST':
+        res = {
+            'code': 0,
+            'msg': "登录成功",
+            'self': None
+        }
         data = request.data
-        print(data)
-        return JsonResponse(data)
+        name = data['username']
+        pwd = data['password']
+        if not name:
+            res['code'] = 1
+            res['msg'] = '请输入用户名'
+            res['self'] = 'name'
+            return JsonResponse(res)
+        if not pwd:
+            res['code'] = 2
+            res['msg'] = '请输入密码'
+            res['self'] = 'pwd'
+            return JsonResponse(res)
+
+        if name != 'admin' or pwd != '12345':
+            res['code'] = 3
+            res['msg'] = '用户名或密码错误'
+            res['self'] = 'pwd'
+            return JsonResponse(res)
+        return JsonResponse(res)
     return render(request, 'login.html')
     
 def Sign(request):
