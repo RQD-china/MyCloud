7af2784f598ca8545a00d7d9d6d6f84f84b09c4d
Richy_D <1245413737@qq.com>
2022-07-22 18:05:20 +0800

    博客主页
    为博客主页添加动画效果

--- app01/admin.py
+++ app01/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from app01.models import Articles
-from app01.models import Tags
-from app01.models import Cover
+from app01.models import Articles, Tags, Cover, MenuImg
 
 # Register your models here.
 
 admin.site.register(Articles)
 admin.site.register(Tags)
 admin.site.register(Cover)
+admin.site.register(MenuImg)
