37cf326c35a72b50d6e83ccd3bf4ce3c68c257e0
Richy_D <1245413737@qq.com>
2022-07-30 21:14:44 +0800

    新闻页面
    完成新闻页面的内容和API对接

--- app01/contexts.py
+++ app01/contexts.py
@@ -1,4 +1,4 @@
 from django.conf import settings
 
 def iconpark(request):
-    return {'iconpark': 'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/icons_17540_14.3c515ebf7780780c386f62ef416ec2fe.js'}
+    return {'iconpark': 'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/icons_17540_15.d52e59f42405023907d574f73f5f96d2.js'}
