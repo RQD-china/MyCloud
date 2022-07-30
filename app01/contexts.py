1c7a0db62a46bd36fb6e93d034995129778eb926
Richy_D <1245413737@qq.com>
2022-07-30 16:59:55 +0800

    新闻页面
    新闻页面框架搭建

---
+++ app01/contexts.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+def iconpark(request):
+    return {'iconpark': 'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/icons_17540_14.3c515ebf7780780c386f62ef416ec2fe.js'}
